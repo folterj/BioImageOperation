131700ab01179b0f7fce6eb2958dee172b43fc6d
Joost de Folter <folterj@gmail.com>
2019-10-23 23:04:41 +0100

    Increased version number

--- BioImageOperation/AssemblyInfo.h
+++ BioImageOperation/AssemblyInfo.h
@@ -25,8 +25,8 @@
 #define Assembly_Description L"http://joostdefolter.info"
 
 // Version
-#define File_Version 1,4,410,20
-#define File_Version_Str "1.4.410.20"
+#define File_Version 1,4,410,21
+#define File_Version_Str "1.4.410.21"
 
 // CLR assembly version
 #define Assembly_Version File_Version_Str
